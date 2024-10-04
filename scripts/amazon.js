ebcb377ff4d485172688fcaf876be67581573313
eakshitha <eakshitha2005@gmail.com>
2024-10-04 10:25:21 +0530

    changes

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -81,8 +81,18 @@ document.querySelectorAll('.jsaddtocart').forEach((button) => {
           quantity: 1
         });
       }
+
+
+      let cartQuantity = 0;
+
+      cart.forEach((item)=>
+    {
+        cartQuantity += item.quantity;
+    });
+    ;
+
+document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
   
-      console.log(cart);
     });
   });
   
