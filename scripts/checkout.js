afb1e1b3eb4c5872e6c666f62a9785c20ab8999b
eakshitha <eakshitha2005@gmail.com>
2024-10-04 11:45:36 +0530

    changes

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -47,7 +47,7 @@ cartSummaryHTML +=
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}>
                     Delete
                   </span>
                 </div>
@@ -103,3 +103,10 @@ cartSummaryHTML +=
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML
 
+document.querySelectorAll('.js-delete-link').forEach((link)=>
+{
+  link.addEventListener('click',()=>
+  {
+     const productId = link.dataset.productId;
+  });
+});
